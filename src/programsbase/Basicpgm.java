68463e52e660396e51f06f2d98ed7894e1d3dd0a
lumin <ashwathybasil@gmail.com>
2023-12-19 12:03:42 +0530

    first commit

---
+++ src/programsbase/Basicpgm.java
@@ -0,0 +1,14 @@
+package programsbase;
+
+public class Basicpgm {
+
+	public static void main(String[] args) {
+		System.out.println("luminar");
+		System.out.print("technolab");
+		System.out.println("kakkanad");
+		
+		
+
+	}
+
+}
